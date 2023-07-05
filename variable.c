b06a7f01b151a429051dc1d0150ea90d71c54cc0
bhatt mansi <137812671+mansibhatt024@users.noreply.github.com>
2023-07-05 11:29:14 +0530

    Add files via upload

---
+++ variable.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+
+{
+		
+	printf("enter the value:\na:");
+	scanf("%d, &a");
+	printf("b:");
+	scanf("%d, &b");
+	printf("\nAddition of the a and b is: %d");
+	printf("\nSubtraction: %d");
+	printf("\nModulo: %d" );
+	
+	
+}
