8fb1e161aecf4be801db28c13d2aa855500d5fdf
bhatt mansi <137812671+mansibhatt024@users.noreply.github.com>
2023-07-09 17:00:49 +0530

    8-7-2023

---
+++ Practical/relational operator.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter the value of no.");
+	scanf("%d",&a );
+	if(a>=10)
+	{
+		printf("no. is greater then ten");
+	}
+	else
+	{
+		printf("no. is less then ten");
+	}
+	return 0;
+}
