3e8fc1b6b1ce91a706846823fb6bc5fab4066c05
bhatt mansi <137812671+mansibhatt024@users.noreply.github.com>
2023-07-09 16:54:15 +0530

    06-07-2023

---
+++ Practical/implicite.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	char x='a', y='b';
+	printf("Addition of the a and b is: %c",x+y);
+	int a=45, b=4;
+	printf("\nDivision of the a and b is: %f",(float)a/(float)b);
+	return 0;
+}
