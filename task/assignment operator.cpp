b320f2dfb8328e05b0251658d2202ec72317457d
bhatt mansi <137812671+mansibhatt024@users.noreply.github.com>
2023-07-19 21:47:10 +0530

    6-7-2023

---
+++ task/assignment operator.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a=16;
+	int b=6;
+	printf("\n%d", a=b);
+	printf("\n%d", a+=b);
+	printf("\n%d", a-=b);
+	printf("\n%d", a*=b);
+	printf("\n%d", a/=b);
+	printf("\n%d", a%=b);
+	return 0;
+	
+}
