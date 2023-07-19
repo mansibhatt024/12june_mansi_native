765f847e301267d0abae926ea01294bbbf6ea3a5
bhatt mansi <137812671+mansibhatt024@users.noreply.github.com>
2023-07-19 22:15:17 +0530

    19-7-2023

---
+++ task/looping.cpp
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+	int a,i;
+	i=1;
+	printf("\n1.odd\n2.even");
+	printf("\nenter your choice");
+	scanf("%d", &a);
+	
+	switch(a)
+	{
+	 case 1:
+	 	while(i<=10)
+	 	{
+		 if
+	 	    (i%2!=0)
+	       {
+	       	printf("%d\n",i);}
+	       	i--;
+			   
+	       }
+	       break;
+	 case 2:
+	    while(i<=10)
+	    {
+	    	if
+	    	(i%2==0)
+	    	{
+	    	printf("%d\n",i);}
+	    	i--;
+			
+		}
+		   break;
+		       
+	}
+	
+        
+        
+	
+}
