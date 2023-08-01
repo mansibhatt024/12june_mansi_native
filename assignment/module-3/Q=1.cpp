30f9606a49277dd920effa78b13deb9a763e50a0
bhatt mansi <137812671+mansibhatt024@users.noreply.github.com>
2023-08-01 16:29:54 +0530

    9-7-2023

---
+++ assignment/module-3/Q=1.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+
+{
+	printf("name: bhatt mansi");
+	printf("\nBirthdate: 02/04/2004");
+	printf("\nAge: 19 years");
+	printf("\nAddress:jagnath plot-2,behind big bazar,rajkot");
+	
+}
