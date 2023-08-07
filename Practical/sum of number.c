edb0489c03397a225604191b9a47d6b795dbfc76
bhatt mansi <137812671+mansibhatt024@users.noreply.github.com>
2023-08-07 11:51:58 +0530

    5-8-2023

---
+++ Practical/sum of number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int num, sum = 0;
+ 
+    num = 1532;
+    printf("The number is = %d\n",num);
+
+    while(num!=0){
+        sum += num % 10;
+        num = num / 10;
+    }
+ 
+    printf("Sum: %d\n",sum);
+ 
+    return 0;}
+
