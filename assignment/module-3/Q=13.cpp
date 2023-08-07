71e8b5fc2c0b47cd8bbf77eaaada7f84364767e1
bhatt mansi <137812671+mansibhatt024@users.noreply.github.com>
2023-08-07 11:44:10 +0530

    5-8-2023

---
+++ assignment/module-3/Q=13.cpp
@@ -0,0 +1,13 @@
+//WAP TO PRINT FACTORIAL NUMBER
+#include<stdio.h>  
+int main()    
+{    
+ int i,factorial=1,number;    
+ printf("Enter a number: ");    
+  scanf("%d",&number);    
+    for(i=1;i<=number;i++){    
+      factorial=factorial*i;    
+  }    
+  printf("Factorial of %d is: %d",number,factorial);    
+return 0;  
+}   
