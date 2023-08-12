aebc44b4184b92f3461da92f71fabf4a332a085d
bhatt mansi <137812671+mansibhatt024@users.noreply.github.com>
2023-08-12 17:51:15 +0530

    12-8-2023

---
+++ assignment/module-3/PATTERN 1.cpp
@@ -0,0 +1,19 @@
+//pattern 1
+#include<stdio.h>
+int main()
+{
+    int i, j, n;
+    printf("Enter number of lines of pattern: ");
+    scanf("%d", &n);
+
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%d ", j%2);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ assignment/module-3/PATTERN 2.cpp
@@ -0,0 +1,15 @@
+//PATTERN 2
+#include<stdio.h>
+int main()
+{
+	char i,j,count='A';
+	for(i='A';i<='E';i++)
+	{
+		for(j='A';j<=i;j++){
+			printf("%c ",count);
+			count++;
+		}
+		printf("\n");
+	}
+	return 0;
+}
