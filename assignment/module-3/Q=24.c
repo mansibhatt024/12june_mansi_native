e4e79456b4404ce3d3f69afadf5e18574fa2979b
bhatt mansi <137812671+mansibhatt024@users.noreply.github.com>
2023-08-17 16:25:05 +0530

    12-8-2023

--- assignment/module-3/Q=24.c
+++ assignment/module-3/Q=24.c
@@ -1,42 +1,71 @@
 //WAP to make addition, Subtraction and multiplication of two matrix using2-D Array
 #include <stdio.h>
 int main() {
-  int r, c, a[100][100], b[100][100], sum[100][100], i, j;
-  printf("Enter the number of rows (between 1 and 100): ");
-  scanf("%d", &r);
-  printf("Enter the number of columns (between 1 and 100): ");
-  scanf("%d", &c);
+    int i, j, k;
+    int array1[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
 
-  printf("\nEnter elements of 1st matrix:\n");
-  for (i = 0; i < r; ++i)
-    for (j = 0; j < c; ++j) {
-      printf("Enter element a%d%d: ", i + 1, j + 1);
-      scanf("%d", &a[i][j]);
+    int array2[3][3] = {
+        {9, 8, 7},
+        {6, 5, 4},
+        {3, 2, 1}
+    };
+
+    int sum[3][3];
+    int product[3][3];
+    int difference[3][3];
+    // Matrix addition
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            sum[i][j] = array1[i][j] + array2[i][j];
+        }
+    }
+    // Matrix subtraction
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            difference[i][j] = array1[i][j] - array2[i][j];
+        }
     }
 
-  printf("Enter elements of 2nd matrix:\n");
-  for (i = 0; i < r; ++i)
-    for (j = 0; j < c; ++j) {
-      printf("Enter element b%d%d: ", i + 1, j + 1);
-      scanf("%d", &b[i][j]);
+    // Matrix multiplication
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            product[i][j] = 0;
+            for (k = 0; k < 3; k++) {
+                product[i][j] += array1[i][k] * array2[k][j];
+            }
+        }
     }
 
-  // adding two matrices
-  for (i = 0; i < r; ++i)
-    for (j = 0; j < c; ++j) {
-      sum[i][j] = a[i][j] + b[i][j];
+    printf("Matrix Sum:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
+    printf("Matrix Difference:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            printf("%d ", difference[i][j]);
+        }
+        printf("\n");
     }
 
-  // printing the result
-  printf("\nSum of two matrices: \n");
-  for (i = 0; i < r; ++i)
-    for (j = 0; j < c; ++j) {
-      printf("%d   ", sum[i][j]);
-      if (j == c - 1) {
-        printf("\n\n");
-      }
+    printf("Matrix Product:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            printf("%d ", product[i][j]);
+        }
+        printf("\n");
     }
 
-  return 0;
+    return 0;
 }
+
+     
+
 
