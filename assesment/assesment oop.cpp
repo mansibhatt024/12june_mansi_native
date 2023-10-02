f10cb0e20a765760d7c7bfcc3f61b5a8f54a7894
bhatt mansi <137812671+mansibhatt024@users.noreply.github.com>
2023-10-02 22:24:33 +0530

    13-9-2023

--- assesment/assesment oop.cpp
+++ assesment/assesment oop.cpp
@@ -2,69 +2,64 @@
 //a) Name of the lecturer b) Name of the subject c) Name of course d) Number of lecturers
 #include <iostream>
 #include <string>
+using namespace std;
 
-using namespace std; // Using the standard namespace for easier access to standard library components
-
-class Lecture {
+// Define the LectureDetails class
+class LectureDetails {
 private:
-    std::string lecturerName;
-    std::string subjectName;
-    std::string courseName;
+    string name;
+    string subject;
+    string course;
     int numLectures;
 
 public:
-    // Constructor to initialize data members
-    Lecture() : lecturerName(""), subjectName(""), courseName(""), numLectures(0) {}
+    // Constructor to initialize lecture details
+    LectureDetails() : numLectures(0) {}
 
-    // Method to assign initial values
-    void assignInitialValues(std::string lecturer, std::string subject, std::string course, int num) {
-        lecturerName = lecturer;
-        subjectName = subject;
-        courseName = course;
-        numLectures = num;
+    // Function to add lecture details
+    void addLectureDetails(string lecturerName, string lectureSubject, string lectureCourse, int lectures) {
+        name = lecturerName;
+        subject = lectureSubject;
+        course = lectureCourse;
+        numLectures = lectures;
     }
 
-    // Method to add lecture details
-    void addLectureDetails() {
-        cout << "Enter lecturer name: ";
-        cin.ignore(); // Ignore any previous newline characters in the input buffer
-        getline(cin, lecturerName); // Read a line of text for lecturerName
-
-        cout << "Enter subject name: ";
-        getline(cin, subjectName); // Read a line of text for subjectName
-
-        cout << "Enter course name: ";
-        getline(cin, courseName); // Read a line of text for courseName
-
-        cout << "Enter number of lectures: ";
-        cin >> numLectures; // Read an integer for numLectures
-    }
-
-    // Method to display lecture details
+    // Function to display name and lecture details
     void displayLectureDetails() {
-        cout << "Lecturer Name: " << lecturerName << endl;
-        cout << "Subject Name: " << subjectName << endl;
-        cout << "Course Name: " << courseName << endl;
+        cout << "Lecturer Name: " << name << endl;
+        cout << "Subject: " << subject << endl;
+        cout << "Course: " << course << endl;
         cout << "Number of Lectures: " << numLectures << endl;
+        cout << "--------------------" << endl;
     }
 };
 
 int main() {
-    Lecture lecturers[5]; // Create an array of Lecture objects to handle 5 lecturers
+    // Creating lecture details objects for 5 lecturers
+    LectureDetails lecturers[5];
 
+    // Taking input for lecturer details and adding them to the objects
     for (int i = 0; i < 5; ++i) {
-        cout << "Enter details for lecturer " << i + 1 << ":" << endl;
-        lecturers[i].addLectureDetails(); // Call the addLectureDetails method to input data
-        cout << endl;
+        string name, subject, course;
+        int numLectures;
+        cout << "Enter Lecturer Name: ";
+        cin >> name;
+        cout << "Enter Subject: ";
+        cin >> subject;
+        cout << "Enter Course: ";
+        cin >> course;
+        cout << "Enter Number of Lectures: ";
+        cin >> numLectures;
+        lecturers[i].addLectureDetails(name, subject, course, numLectures);
     }
 
-    cout << "Lecture Details:" << endl;
+    // Displaying lecture details for all lecturers
+    cout << "\nLecture Details:" << endl;
     for (int i = 0; i < 5; ++i) {
-        cout << "Lecturer " << i + 1 << ":\n";
-        lecturers[i].displayLectureDetails(); // Call the displayLectureDetails method to display data
-        cout << endl;
+        lecturers[i].displayLectureDetails();
     }
 
     return 0;
 }
+
 
